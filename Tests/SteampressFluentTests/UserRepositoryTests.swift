0bd7fb9f120d4c89fb2579651c9ac56bcc5d6d49
Tim <0xtimc@gmail.com>
2020-04-17 11:53:21 +0100

    Get user repository tests compiling

--- Tests/SteampressFluentTests/UserRepositoryTests.swift
+++ Tests/SteampressFluentTests/UserRepositoryTests.swift
@@ -28,14 +28,14 @@ class UserRepositoryTests: XCTestCase {
         
         XCTAssertNotNil(savedUser.userID)
         
-        let userFromDB = try BlogUser.query(on: connection).filter(\.userID == savedUser.userID).first().wait()
+        let userFromDB = try BlogUser.query(on: app.db).filter(\.userID == savedUser.userID).first().wait()
         XCTAssertEqual(userFromDB?.username, newUser.username)
     }
     
     func testGetingAUserByUsername() throws {
         let username = "alice"
-        let newUser = BlogUser(name: "Alice", username: username, password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil)
-        _ = try newUser.save(on: connection).wait()
+        let newUser = FluentBlogUser(userID: nil, name: "Alice", username: username, password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil)
+        try newUser.save(on: app.db).wait()
 
         let retrievedUser = try repository.getUser(username: username, on: app).wait()
 
@@ -44,31 +44,32 @@ class UserRepositoryTests: XCTestCase {
     }
     
     func testGettingAUserByID() throws {
-        let newUser = try BlogUser(name: "Alice", username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
+        let newUser = FluentBlogUser(userID: nil, name: "Alice", username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil)
+        try newUser.save(on: app.db).wait()
         
-        let retrievedUser = try repository.getUser(id: newUser.userID!, on: app).wait()
+        let retrievedUser = try repository.getUser(id: newUser.id!).wait()
         
         XCTAssertEqual(retrievedUser?.username, newUser.username)
     }
     
     func testGettingAUserByName() throws {
         let name = "Alice"
-        let newUser = BlogUser(name: name, username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil)
-        _ = try newUser.save(on: connection).wait()
+        let newUser = FluentBlogUser(userID: nil, name: name, username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil)
+        try newUser.save(on: app.db).wait()
 
-        let retrievedUser = try repository.getUser(name: name, on: app).wait()
+        let retrievedUser = try repository.getUser(name: name).wait()
 
         XCTAssertEqual(retrievedUser?.name, name)
-        XCTAssertEqual(retrievedUser?.userID, newUser.userID)
+        XCTAssertEqual(retrievedUser?.userID, newUser.id)
     }
     
     func testGettingAllUsers() throws {
         let name1 = "Alice"
         let name2 = "Bob"
-        _ = try BlogUser(name: name1, username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
-        _ = try BlogUser(name: name2, username: "bob", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
+        try FluentBlogUser(userID: nil, name: name1, username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: app.db).wait()
+        try FluentBlogUser(userID: nil, name: name2, username: "bob", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: app.db).wait()
         
-        let users = try repository.getAllUsers(on: app).wait()
+        let users = try repository.getAllUsers().wait()
 
         XCTAssertEqual(users.count, 2)
         XCTAssertEqual(users.first?.name, name1)
@@ -76,32 +77,33 @@ class UserRepositoryTests: XCTestCase {
     }
     
     func testUsersCount() throws {
-        _ = try BlogUser(name: "Alice", username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
-        _ = try BlogUser(name: "Bob", username: "bob", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
+        try FluentBlogUser(userID: nil, name: "Alice", username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: app.db).wait()
+        try FluentBlogUser(userID: nil, name: "Bob", username: "bob", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: app.db).wait()
         
-        let count = try repository.getUsersCount(on: app).wait()
+        let count = try repository.getUsersCount().wait()
         
         XCTAssertEqual(count, 2)
     }
     
     func testDeletingAUser() throws {
-        let user = try BlogUser(name: "Alice", username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
+        let user = BlogUser(userID: nil, name: "Alice", username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil)
+        try user.toFluentUser().save(on: app.db).wait()
         
-        let count = try BlogUser.query(on: connection).count().wait()
+        let count = try FluentBlogUser.query(on: app.db).count().wait()
         XCTAssertEqual(count, 1)
         
-        try repository.delete(user, on: app).wait()
+        try repository.delete(user).wait()
         
-        let countAfterDelete = try BlogUser.query(on: connection).count().wait()
+        let countAfterDelete = try FluentBlogUser.query(on: app.db).count().wait()
         XCTAssertEqual(countAfterDelete, 0)
     }
     
     func testCreatingUserWithExistingUsernameFails() throws {
         let username = "alice"
         var errorOccurred = false
-        _ = try BlogUser(name: "Alice", username: username, password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
+        try FluentBlogUser(userID: nil, name: "Alice", username: username, password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: app.db).wait()
         do {
-            _ = try BlogUser(name: "Bob", username: username, password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
+            try FluentBlogUser(userID: nil, name: "Bob", username: username, password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: app.db).wait()
         } catch {
             errorOccurred = true
         }
@@ -110,18 +112,21 @@ class UserRepositoryTests: XCTestCase {
     }
     
     func testGettingUsersWithPostCounts() throws {
-        let postAuthor = try BlogUser(name: "Alice", username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
-        let otherUser = try BlogUser(name: "Bob", username: "bob", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
-        let newUser = try BlogUser(name: "Luke", username: "luke", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil).save(on: connection).wait()
+        let postAuthor = FluentBlogUser(userID: nil, name: "Alice", username: "alice", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil)
+        try postAuthor.save(on: app.db).wait()
+        let otherUser = FluentBlogUser(userID: nil, name: "Bob", username: "bob", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil)
+        try otherUser.save(on: app.db).wait()
+        let newUser = FluentBlogUser(userID: nil, name: "Luke", username: "luke", password: "password", profilePicture: nil, twitterHandle: nil, biography: nil, tagline: nil)
+        try newUser.save(on: app.db).wait()
         
-        _ = try BlogPost(title: "A new post", contents: "Some Contents about vapor", author: postAuthor, creationDate: Date().addingTimeInterval(-360), slugUrl: "a-new-post", published: true).save(on: connection).wait()
-        _ = try BlogPost(title: "A different Vapor post", contents: "Some other contents", author: postAuthor, creationDate: Date().addingTimeInterval(360), slugUrl: "a-different-post", published: true).save(on: connection).wait()
-        _ = try BlogPost(title: "A third post", contents: "Some other contents containing vapor", author: postAuthor, creationDate: Date(), slugUrl: "a-third-post", published: true).save(on: connection).wait()
-        _ = try BlogPost(title: "A draft Vapor post", contents: "Some other contents", author: postAuthor, creationDate: Date().addingTimeInterval(60), slugUrl: "a-draft-post", published: true).save(on: connection).wait()
-        _ = try BlogPost(title: "An unrelated draft post", contents: "Some other contents", author: postAuthor, creationDate: Date().addingTimeInterval(10), slugUrl: "an-unrelated-draft-post", published: false).save(on: connection).wait()
-        _ = try BlogPost(title: "An unrelated post", contents: "Some other contents", author: otherUser, creationDate: Date().addingTimeInterval(30), slugUrl: "an-unrelated-post", published: true).save(on: connection).wait()
+        try FluentBlogPost(id: nil, title: "A new post", contents: "Some Contents about vapor", author: postAuthor.requireID(), creationDate: Date().addingTimeInterval(-360), slugUrl: "a-new-post", published: true).save(on: app.db).wait()
+        try FluentBlogPost(id: nil, title: "A different Vapor post", contents: "Some other contents", author: postAuthor.requireID(), creationDate: Date().addingTimeInterval(360), slugUrl: "a-different-post", published: true).save(on: app.db).wait()
+        try FluentBlogPost(id: nil, title: "A third post", contents: "Some other contents containing vapor", author: postAuthor.requireID(), creationDate: Date(), slugUrl: "a-third-post", published: true).save(on: app.db).wait()
+        try FluentBlogPost(id: nil, title: "A draft Vapor post", contents: "Some other contents", author: postAuthor.requireID(), creationDate: Date().addingTimeInterval(60), slugUrl: "a-draft-post", published: true).save(on: app.db).wait()
+        try FluentBlogPost(id: nil, title: "An unrelated draft post", contents: "Some other contents", author: postAuthor.requireID(), creationDate: Date().addingTimeInterval(10), slugUrl: "an-unrelated-draft-post", published: false).save(on: app.db).wait()
+        try FluentBlogPost(id: nil, title: "An unrelated post", contents: "Some other contents", author: otherUser.requireID(), creationDate: Date().addingTimeInterval(30), slugUrl: "an-unrelated-post", published: true).save(on: app.db).wait()
         
-        let usersWithPostCount = try repository.getAllUsersWithPostCount(on: app).wait()
+        let usersWithPostCount = try repository.getAllUsersWithPostCount().wait()
         XCTAssertEqual(usersWithPostCount.count, 3)
         XCTAssertEqual(usersWithPostCount.first?.1, 4)
         XCTAssertEqual(usersWithPostCount.first?.0.username, postAuthor.username)
@@ -133,8 +138,7 @@ class UserRepositoryTests: XCTestCase {
     
     func testAdminUserMigrationsCreatesAdminUser() throws {
         app = try TestSetup.getApp(enableAdminUser: true)
-        connection = try app.requestPooledConnection(to: .psql).wait()
-        let user = try XCTUnwrap(BlogUser.query(on: connection).first().wait())
+        let user = try XCTUnwrap(FluentBlogUser.query(on: app.db).first().wait())
         XCTAssertEqual(user.username, "admin")
         XCTAssertEqual(user.name, "Admin")
     }
